a42a1e0335d4d93d2b9fa55a518591fbf7a8d606
Scott Fritzinger <scott@talageins.com>
2020-04-22 15:59:07 -0700

    Adding __location function to print both the filename and line number in one call

--- tracker.js
+++ tracker.js
@@ -33,3 +33,9 @@ Object.defineProperty(global, '__file', {
 		return __stack[1].getFileName().split('/').slice(-1)[0].split('.').slice(0, -1).join('.');
 	}
 });
+
+Object.defineProperty(global, '__location', {
+	get: function () {
+		return 'fileName: ' + __stack[1].getFileName().split('/').slice(-1)[0].split('.').slice(0, -1).join('.') + ' lineNumber: ' + __stack[1].getLineNumber();
+	}
+});
