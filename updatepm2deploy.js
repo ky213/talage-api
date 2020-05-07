56c9bb19c987ac7672aeb8218c951b376acad8ae
Scott Fritzinger <scott@talageins.com>
2020-05-07 11:41:00 -0700

    Reverting to original versions of updatepm2deploy, tracker, and codecheck

--- updatepm2deploy.js
+++ updatepm2deploy.js
@@ -1,73 +1,71 @@
 /* eslint-disable */
 
-const metadata = require('node-ec2-metadata');
-const Q = require('q');
-const fs = require('fs');
-const inputFile = 'pm2.json';
-const outputFile = 'pm2deployed.json';
-// Load pm2.json
-const pm2json = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
+var metadata = require('node-ec2-metadata');
+var Q = require('q');
+var fs = require('fs');
+var inputFile = 'pm2.json'
+var outputFile = 'pm2deployed.json'
+//load pm2.json
+let pm2json = JSON.parse(fs.readFileSync(inputFile, 'utf8'));
 
-const appJSON = pm2json.apps[0];
+let appJSON = pm2json.apps[0];
 if(process.env.NODE_ENV){
-	appJSON.env.NODE_ENV = process.env.NODE_ENV;
+    appJSON.env.NODE_ENV = process.env.NODE_ENV;
 }
-if(process.env.USER === 'ec2-user'){
-	metadata.isEC2().
-		then(function(onEC2){
-			console.log(`Running on EC2? ${onEC2}`);
-			if(onEC2 === true){
-				Q.all([
-					metadata.getMetadataForInstance('ami-id'),
-					metadata.getMetadataForInstance('hostname'),
-					metadata.getMetadataForInstance('public-hostname'),
-					metadata.getMetadataForInstance('public-ipv4'),
-					metadata.getMetadataForInstance('instance-id')
-				]).
-					spread(function(amiID, hostname, publicHostname, publicIPv4, instanceid){
-						// AppJSON.env.AMP_ID = amiID
-						appJSON.env.INSTANCE_ID = instanceid;
-						appJSON.env.HOSTNAME = hostname;
-						appJSON.env.PUBLIC_HOSTNAME = publicHostname;
-						appJSON.env.PUBLIC_IPV4 = publicIPv4;
+if (process.env.USER === "ec2-user") {
+    metadata.isEC2()
+        .then(function (onEC2) {
+            console.log("Running on EC2? " + onEC2);
+            if (onEC2 === true) {
+                Q.all([
+                    metadata.getMetadataForInstance('ami-id'),
+                    metadata.getMetadataForInstance('hostname'),
+                    metadata.getMetadataForInstance('public-hostname'),
+                    metadata.getMetadataForInstance('public-ipv4'),
+                    metadata.getMetadataForInstance('instance-id'),
+                ])
+                    .spread(function (amiID, hostname, publicHostname, publicIPv4, instanceid) {
+                        //appJSON.env.AMP_ID = amiID
+                        appJSON.env.INSTANCE_ID = instanceid
+                        appJSON.env.HOSTNAME = hostname
+                        appJSON.env.PUBLIC_HOSTNAME = publicHostname
+                        appJSON.env.PUBLIC_IPV4 = publicIPv4
 
-						/*
-						 * Console.log("AMI-ID: " + amiID);
-						 * Console.log("Hostname: " + hostname);
-						 * Console.log("Public Hostname: " + publicHostname);
-						 * Console.log("Public IPv4: " + publicIPv4);
-						 */
+                        // console.log("AMI-ID: " + amiID);
+                        // console.log("Hostname: " + hostname);
+                        // console.log("Public Hostname: " + publicHostname);
+                        // console.log("Public IPv4: " + publicIPv4);
 
-						/*
-						 * Var defaultMetaData = {
-						 *     AmiID: amiID,
-						 *     Hostname: hostname,
-						 *     PublicHostname: publicHostname,
-						 *     PUBLIC_IPV4: publicIPv4
-						 * }
-						 * update pm2 json..
-						 */
-						savepm2deployed(pm2json);
-					}).
-					fail(function(error){
-						console.log(`Error: ${error}`);
-						process.exit(1);
-					});
-			}
-		});
-}else{
-	// Local dev or not EC2 AWS Linux
-	appJSON.env.INSTANCE_ID = 'Not EC2 - localdev';
-	appJSON.env.HOSTNAME = 'local';
-	appJSON.env.PUBLIC_HOSTNAME = 'localhost';
-	appJSON.env.PUBLIC_IPV4 = 'localhost';
-	savepm2deployed(pm2json);
+                        // var defaultMetaData = {
+                        //     amiID: amiID,
+                        //     hostname: hostname,
+                        //     publicHostname: publicHostname,
+                        //     PUBLIC_IPV4: publicIPv4
+                        // }
+                        //update pm2 json..                        
+                        savepm2deployed(pm2json);
+                    })
+                    .fail(function (error) {
+                        console.log("Error: " + error);
+                        process.exit(1);
+                    });
+            }
+        });
+}
+else {
+    //local dev or not EC2 AWS Linux  
+    appJSON.env.INSTANCE_ID = "Not EC2 - localdev"
+    appJSON.env.HOSTNAME = "local"
+    appJSON.env.PUBLIC_HOSTNAME = "localhost"
+    appJSON.env.PUBLIC_IPV4 = "localhost"
+    savepm2deployed(pm2json);
 
 }
 
 
-function savepm2deployed(pm2json){
-	fs.writeFileSync(outputFile, JSON.stringify(pm2json, null, 2));
-	process.exit(0);
+
+function savepm2deployed(pm2json) {
+    fs.writeFileSync(outputFile, JSON.stringify(pm2json, null, 2));
+    process.exit(0);
 
 }
