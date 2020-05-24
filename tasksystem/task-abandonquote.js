1ab7a8b80bd4e17aa644ee2c65573b24ac6bcb15
Brian Pullin <brian@pullindev.com>
2020-05-23 17:11:12 -0700

    Abandon Quote agency email working.

--- tasksystem/task-abandonquote.js
+++ tasksystem/task-abandonquote.js
@@ -114,44 +114,43 @@ var processAbandonQuote = async function (applicationId){
     //get   
     const quoteAppSQL = `
         SELECT
-            ${db.quoteName('a.agency_location', 'agencyLocation')},
-            ${db.quoteName('a.wholesale')},
-            ${db.quoteName('ag.agency_network')},
-            ${db.quoteName('ag.name', 'agencyName')},
-            ${db.quoteName('ag.website', 'agencyWebsite')},
-            ${db.quoteName('al.agency')},
-            ${db.quoteName('al.email', 'agencyEmail')},
-            ${db.quoteName('al.phone', 'agencyPhone')},
-            ${db.quoteName('b.name', 'businessName')},
-            ${db.quoteName('c.email')},
-            ${db.quoteName('c.fname')},
-            ${db.quoteName('c.lname')},
-            ${db.quoteName('c.phone')},
-            ${db.quoteName('i.agent_login')},
-            ${db.quoteName('i.logo')},
-            ${db.quoteName('i.name', 'insurer')},
-            ${db.quoteName('ic.description', 'industryCode')},
-            ${db.quoteName('pt.name', 'policyType')},
-            ${db.quoteName('q.amount')},
-            ${db.quoteName('q.api_result')},
-            ${db.quoteName('q.application')},
-            ${db.quoteName('q.number')},
-            ${db.quoteName('an.email_brand', 'emailBrand')}
-        FROM ${db.quoteName('#__applications', 'a')}
-            LEFT JOIN ${db.quoteName('#__quotes', 'q')} ON ${db.quoteName('a.id')} = ${db.quoteName('q.application')}
-            LEFT JOIN ${db.quoteName('#__businesses', 'b')} ON ${db.quoteName('b.id')} = ${db.quoteName('a.business')}
-            LEFT JOIN ${db.quoteName('#__insurers', 'i')} ON ${db.quoteName('q.insurer')} = ${db.quoteName('i.id')}
-            LEFT JOIN ${db.quoteName('#__policy_types', 'pt')} ON ${db.quoteName('q.policy_type')} = ${db.quoteName('pt.abbr')}
-            LEFT JOIN ${db.quoteName('#__contacts', 'c')} ON ${db.quoteName('a.business')} = ${db.quoteName('c.business')}
-            LEFT JOIN ${db.quoteName('#__industry_codes', 'ic')} ON ${db.quoteName('a.industry_code')} = ${db.quoteName('ic.id')}
-            LEFT JOIN ${db.quoteName('#__agency_locations', 'al')} ON ${db.quoteName('a.agency_location')} = ${db.quoteName('al.id')}
-            LEFT JOIN ${db.quoteName('#__agencies', 'ag')} ON ${db.quoteName('al.agency')} = ${db.quoteName('ag.id')}
-            LEFT JOIN ${db.quoteName('#__agency_networks', 'an')} ON ${db.quoteName('ag.agency_network')} = ${db.quoteName('an.id')}
+            a.agency_location AS agencyLocation,
+            a.wholesale,
+            ag.agency_network,
+            ag.name AS agencyName,
+            ag.website AS agencyWebsite,
+            al.agency,
+            al.email AS agencyEmail,
+            al.phone AS agencyPhone,
+            b.name AS businessName,
+            c.email,
+            c.fname,
+            c.lname,
+            c.phone,
+            i.agent_login,
+            i.logo,
+            i.name AS insurer,
+            ic.description AS industryCode,
+            pt.name AS policyType,
+            q.amount,
+            q.api_result,
+            q.application,
+            q.number,
+            an.email_brand AS emailBrand
+        FROM clw_talage_applications AS a
+            LEFT JOIN clw_talage_quotes AS q ON a.id = q.application
+            LEFT JOIN clw_talage_businesses AS b ON b.id = a.business
+            LEFT JOIN clw_talage_insurers AS i ON q.insurer = i.id
+            LEFT JOIN clw_talage_policy_types AS pt ON q.policy_type = pt.abbr
+            LEFT JOIN clw_talage_contacts AS c ON a.business = c.business
+            LEFT JOIN clw_talage_industry_codes AS ic ON a.industry_code = ic.id
+            LEFT JOIN clw_talage_agency_locations AS al ON a.agency_location = al.id
+            LEFT JOIN clw_talage_agencies AS ag ON al.agency = ag.id
+            LEFT JOIN clw_talage_agency_networks AS an ON ag.agency_network = an.id
         WHERE 
-        ${db.quoteName('a.id')} = ${applicationId}
+        a.id =  ${applicationId}
     `;
     
-    //log.debug('SQL: ' + quoteAppSQL);
     let quotes = null;
 	try {
         quotes = await db.query(quoteAppSQL);
@@ -161,8 +160,6 @@ var processAbandonQuote = async function (applicationId){
         // Do not throw error other abandon quotes may need to be processed.
         return false;
     }
-    // log.debug('quotes:')
-    // log.debug(JSON.stringify(quotes));
 
     if(quotes && quotes.length > 0){
        
@@ -170,16 +167,16 @@ var processAbandonQuote = async function (applicationId){
         //Get email content
         const emailContentSQL = `
             SELECT
-                JSON_EXTRACT(\`custom_emails\`, '$.abandoned_quotes_agency') AS agencyEmailData,
-                JSON_EXTRACT(\`custom_emails\`, '$.abandoned_quotes_customer') AS customerEmailData,
-                (SELECT JSON_EXTRACT(\`custom_emails\`, '$.abandoned_quotes_agency')  FROM  ${db.quoteName('#__agency_networks')} WHERE id = 1 ) AS defaultAgencyEmailData,
-                (SELECT JSON_EXTRACT(\`custom_emails\`, '$.abandoned_quotes_customer')  FROM  ${db.quoteName('#__agency_networks')} WHERE id = 1 ) AS defaultCustomerEmailData
-            FROM \`#__agency_networks\`
-            WHERE \`id\` = ${db.escape(agencyNetwork)}
-            ORDER BY \`id\` DESC
-            LIMIT 2;
+                JSON_EXTRACT(custom_emails, '$.abandoned_quotes_agency') AS agencyEmailData,
+                JSON_EXTRACT(custom_emails, '$.abandoned_quotes_customer') AS customerEmailData,
+                (SELECT JSON_EXTRACT(custom_emails, '$.abandoned_quotes_agency')  FROM  clw_talage_agency_networks WHERE id = 1 ) AS defaultAgencyEmailData,
+                (SELECT JSON_EXTRACT(custom_emails, '$.abandoned_quotes_customer')  FROM  clw_talage_agency_networks WHERE id = 1 ) AS defaultCustomerEmailData
+            FROM clw_talage_agency_networks
+            WHERE id = ${db.escape(agencyNetwork)}
+            ORDER BY id DESC
+            LIMIT 2; 
         `;
-        //log.debug('email sql ' + emailContentSQL );
+       
         let error = null;
         const emailContentResultArray = await db.query(emailContentSQL).catch(function(err){
             log.error(`DB Error Unable to get email content for abandon quote. appid: ${applicationId}.  error: ${err}`);
@@ -188,8 +185,6 @@ var processAbandonQuote = async function (applicationId){
         if(error){
             return false;
         }
-        //log.debug('email content')
-        //log.debug(JSON.stringify(emailContentResult));
             
         if(emailContentResultArray && emailContentResultArray.length >0 ){
             const emailContentResult = emailContentResultArray[0];
@@ -202,15 +197,9 @@ var processAbandonQuote = async function (applicationId){
             let customerEmailData = emailContentResult.customerEmailData ? JSON.parse(emailContentResult.customerEmailData) : null;
             let defaultCustomerEmailData = emailContentResult.defaultCustomerEmailData ? JSON.parse(emailContentResult.defaultCustomerEmailData) : null;
 
-
-         
-
             let agencyName = quotes[0].agencyName
             let agencyPhone = '';
             let brand = quotes[0].emailBrand === 'wheelhouse' ? 'agency' : quotes[0].emailBrand.agency;
-            log.debug('quotes[0].emailBrand '+ quotes[0].emailBrand );
-            log.debug('quotes[0].emailBrand.agency ' + quotes[0].emailBrand.agency);
-            log.debug('brand: ' + brand);
 
             if(quotes[0].wholesale || quotes[0].agency == 1 || quotes[0].agency == 2){
                 // Override the agency info with Talage
@@ -249,8 +238,6 @@ var processAbandonQuote = async function (applicationId){
             subject = customerEmailData && customerEmailData.subject ? customerEmailData.subject : defaultCustomerEmailData.subject;
            
             // Perform content replacements
-            // Perform content replacements
-            log.debug('agencyName: ' + agencyName);
             message = message.replace(/{{Agency}}/g, agencyName);
             message = message.replace(/{{Agency Email}}/g, quotes[0].agencyEmail);
             message = message.replace(/{{Agency Phone}}/g, agencyPhone);
@@ -262,38 +249,65 @@ var processAbandonQuote = async function (applicationId){
             subject = subject.replace(/{{is\/are}}/g, quotes.length === 1 ? 'is' : 'are');
             subject = subject.replace(/{{s}}/g, quotes.length === 1 ? '' : 's');
 
-            log.debug('subject: ' + subject)
-            log.debug('message: ' + message)
+            log.debug('Insured subject: ' + subject)
+            log.debug('Insured message: ' + message)
 
             //send email:
             // Send the email
-            // if(email.sendEmail(quote.agencyEmail, subject, message, {'application': appID, 'agency_location': quote.agencyLocation}, quote.emailBrand)){
-            //     slack.sendSlackMessage('#alerts', 'warning','The system failed to inform an agency of the abandoned quote' + (quotes.length === 1 ? '' : 's') + " for application #appID. Please follow-up manually. application_id " + applicationId);
-            // }
+            if(email.send(quotes[0].email, subject, message, {'application': applicationId, 'agency_location': quotes[0].agencyLocation}, quotes[0].emailBrand)){
+               slack.sendSlackMessage('#alerts', 'warning','The system failed to remind the insured to revisit their quotes for application #${applicationId}. Please follow-up manually.' );
+            }
 
             /* ---=== Email to Agency (not sent to Talage) ===--- */
 
             // Only send for non-Talage accounts that are not wholesale
-            // if(!$quotes[0]->wholesale && $quotes[0]->agency !== 1){
-            // }
+            log.debug('Wholesale: ' + quotes[0].wholesale);
+            if(quotes[0].wholesale == false && quotes[0].agency !== 1){
+               
+                quotes[0].businessName =  await crypt.decrypt(quotes[0].businessName);
+                quotes[0].fname =  await crypt.decrypt(quotes[0].fname);
+                quotes[0].lname =  await crypt.decrypt(quotes[0].lname);
+                quotes[0].phone =  await crypt.decrypt(quotes[0].phone);
 
-            // let agencyEmailData = emailContentResult.agencyEmailData ? JSON.parse(emailContentResult.agencyEmailData) : null;
-            // let defaultAgencyEmailData = emailContentResult.defaultAgencyEmailData ? JSON.parse(emailContentResult.defaultAgencyEmailData) : null;
-            //  // Determine which message and subject to use
-            //  message = agencyEmailData && agencyEmailData.message  ? agencyEmailData.message : defaultAgencyEmailData.message;
-            //  subject = agencyEmailData && agencyEmailData.subject ? agencyEmailData.subject : defaultAgencyEmailData.subject;
-            
-            //  // Perform content message.replacements
-            //  message = message.replace(/Agent Login URL/g, quotes[0].agent_login);
-            //  message = message.replace(/Agency Portal/g, "<a href=\"portalLink\" rel=\"noopener noreferrer\" target=\"_blank\">Agency Portal</a>");
-            //  message = message.replace(/Brand/g, ucwords(quotes[0].emailBrand));
-            //  message = message.replace(/Business Name/g, quotes[0].businessName);
-            //  message = message.replace(/Contact Email/g, quotes[0].email);
-            //  message = message.replace(/Contact Name/g, fullName);
-            //  message = message.replace(/Contact Phone/g, phone);
-            //  message = message.replace(/Industry/g, quotes[0].industryCode);
-            //  message = message.replace(/Quotes/g, quotesHTML);
+                let agencyEmailData = emailContentResult.agencyEmailData ? JSON.parse(emailContentResult.agencyEmailData) : null;
+                let defaultAgencyEmailData = emailContentResult.defaultAgencyEmailData ? JSON.parse(emailContentResult.defaultAgencyEmailData) : null;
+                
+                const portalLink = global.settings.PORTAL_URL;
+                
+                // Format the full name and phone number
+                fullName = stringFunctions.ucwords(stringFunctions.strtolower(quotes[0].fname) + ' ' + stringFunctions.strtolower(quotes[0].lname));
+                phone = '';
+                if(quotes[0].phone){
+                    phone = formatPhone(quotes[0].phone);;
+                }
 
+                //  // Determine which message and subject to use
+                 message = agencyEmailData && agencyEmailData.message  ? agencyEmailData.message : defaultAgencyEmailData.message;
+                 subject = agencyEmailData && agencyEmailData.subject ? agencyEmailData.subject : defaultAgencyEmailData.subject;
+                
+
+                //already have quotesHTML from above
+
+                //  // Perform content message.replacements
+                message = message.replace(/{{Agent Login URL}}/g, quotes[0].agent_login);
+                 message = message.replace(/{{Agency Portal}}/g, `<a href=\"${portalLink}\" rel=\"noopener noreferrer\" target=\"_blank\">Agency Portal</a>`);
+                 message = message.replace(/{{Brand}}/g, stringFunctions.ucwords(quotes[0].emailBrand));
+                 message = message.replace(/{{Business Name}}/g, quotes[0].businessName);
+                 message = message.replace(/{{Contact Email}}/g, quotes[0].email);
+                 message = message.replace(/{{Contact Name}}/g, fullName);
+                 message = message.replace(/{{Contact Phone}}/g, phone);
+                 message = message.replace(/{{Industry}}/g, quotes[0].industryCode);
+                 message = message.replace(/{{Quotes}}/g, quotesHTML);
+
+
+                  // Send the email
+                    if(email.send(quotes[0].agencyEmail, subject, message, {'application': applicationId, 'agency_location': quotes[0].agencyLocation}, quote.emailBrand)){
+                        slack.sendSlackMessage('#alerts', 'warning','The system failed to inform an agency of the abandoned quote' + (quotes.length === 1 ? '' : 's') + ' for application #${applicationId}. Please follow-up manually.');
+                    }
+
+                log.debug('Agency subject: ' + subject)
+                log.debug('Agency message: ' + message)
+            }
 
             return true;
         }
