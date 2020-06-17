8dce5a1e2d185f8aa49b1091c8ffc52930dc3908
Brian Pullin <brian@pullindev.com>
2020-06-17 09:07:22 -0700

    email: fixed not sending response.

--- private/v1/email/email.js
+++ private/v1/email/email.js
@@ -85,12 +85,16 @@ async function PostEmail(req, res, next){
 	//call email service
 	const respSendEmail = await emailSvc.send(req.body.to, req.body.subject, req.body.html, req.body.keys, req.body.brand, req.body.agency, req.body.attachments).catch(function(err){
 		log.error("Send email error: " + err + __location);
-		return next(serverHelper.requestError(err));
+		return res.send(serverHelper.internalError("SendEmail Error"));
 	});
 	if(respSendEmail === false){
 		log.error("Send email error response was false: " + __location);
-		return next(serverHelper.requestError("SendEmail Error"));
+		return res.send(serverHelper.internalError("SendEmail Error"));
 	}
+	res.send(200, {
+		'message': 'Email sent',
+		'status': 'success'
+	});
 	return next();
 }
 
