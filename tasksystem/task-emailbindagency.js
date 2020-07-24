b9a8f52ab38b54d17804febb57c7d4ca1d87b440
Brian Pullin <brian@pullindev.com>
2020-07-23 19:29:02 -0700

    post merge fix for undefined.

--- tasksystem/task-emailbindagency.js
+++ tasksystem/task-emailbindagency.js
@@ -226,7 +226,7 @@ var emailbindagency = async function(applicationId, quoteId) {
 
                     //log.debug("sending customer email " + __location);
                     const brand = applications[0].emailBrand === 'wheelhouse' ? 'agency' : `${applications[0].emailBrand}-agency`
-                    const emailResp2 = await email.send(applications[0].email, subject, message, keyData, brand, applications[0].agencyId);
+                    const emailResp2 = await emailSvc.send(applications[0].email, subject, message, keyData, brand, applications[0].agencyId);
                     // log.debug("emailResp = " + emailResp);
                     if (emailResp2 === false) {
                         slack.send('#alerts', 'warning', `Failed to send Policy Bind Email to Insured application #${applicationId} and quote ${quoteId}. Please follow-up manually.`);
