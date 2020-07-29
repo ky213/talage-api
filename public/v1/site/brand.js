08c70d75795094436c280de8f739e1f392b6d17b
Brian Pullin <brian@pullindev.com>
2020-07-29 13:23:19 -0700

    changed to use img.talageins.com for logos

--- public/v1/site/brand.js
+++ public/v1/site/brand.js
@@ -27,7 +27,7 @@ function PostBrand(req, res, next){
 				'env': 'production',
 				'portalURL': 'https://agents.talageins.com',
 				's3Bucket': 'website-images-staging',
-				'siteURL': 'https://www.talageins.com'
+				'siteURL': 'https://img.talageins.com'
 			});
 			break;
 		case 'alpine.talageins.com':
