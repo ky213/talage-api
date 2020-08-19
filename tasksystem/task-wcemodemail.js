40eab15ffeccc57262cce3bf3200d7783f615cbb
Ryan Young <ryan@talageins.com>
2020-08-19 15:20:27 -0700

    added a period because grammar

--- tasksystem/task-wcemodemail.js
+++ tasksystem/task-wcemodemail.js
@@ -148,7 +148,7 @@ const sendEmodEmail = async function(applicationId) {
             const portalLink = applications[0].agencyNetwork === 1 ? global.settings.PORTAL_URL : global.settings.DIGALENT_AGENTS_URL;
 
             let message = `
-                <p>{{Business Owner}} tried to get a quote in {{STATE}} but they were declined due to E-Mod rating issues. The quote is in your E-Link portal, and can be accessed by you and potentially still be bound, but you will have to verify the E-Mod information</p>
+                <p>{{Business Owner}} tried to get a quote in {{STATE}} but they were declined due to E-Mod rating issues. The quote is in your E-Link portal, and can be accessed by you and potentially still be bound, but you will have to verify the E-Mod information.</p>
                 <ul>
                 <li><b>Business Name</b>: {{Business Name}}</li>
                 <li><b>Quote Number</b>: {{Quote Number}}</li>
