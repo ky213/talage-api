a6a7efef1514f6e0d6c04d2621092e8162792036
Brian Pullin <brian@pullindev.com>
2020-08-26 08:49:11 -0700

    Admin Insurer-writer endpoint

--- public/v1/v1-endpoints.js
+++ public/v1/v1-endpoints.js
@@ -118,10 +118,11 @@ exports.registerEndpoints = (server) => {
     registerEndpoint(server, 'administration', 'message');
     registerEndpoint(server, 'administration', 'insurer');
     registerEndpoint(server, 'administration', 'insurer-contact-rt');
+    registerEndpoint(server, 'administration', 'insurer-outage');
     registerEndpoint(server, 'administration', 'insurer-payment-plan-rt');
     registerEndpoint(server, 'administration', 'insurer-policy-type-rt');
     registerEndpoint(server, 'administration', 'insurer-territory-rt');
-    registerEndpoint(server, 'administration', 'insurer-outage');
+    registerEndpoint(server, 'administration', 'insurer-writer-rt');
     registerEndpoint(server, 'administration', 'agency-network-rt');
     registerEndpoint(server, 'administration', 'agency-location-rt');
     registerEndpoint(server, 'administration', 'territory-rt');
