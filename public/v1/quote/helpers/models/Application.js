ce1c9025d7f62720d1a0db92480c343e09f572e0
Brian Pullin <brian@pullindev.com>
2020-09-12 11:31:02 -0700

    linting

--- public/v1/quote/helpers/models/Application.js
+++ public/v1/quote/helpers/models/Application.js
@@ -545,7 +545,8 @@ module.exports = class Application {
                         short: false,
                         title: 'Business Name',
                         value: this.business.name + (this.business.dba ? ` (dba. ${this.business.dba})` : '')
-                    }, {
+                    },
+                    {
                         short: false,
                         title: 'Industry',
                         value: this.business.industry_code_description
