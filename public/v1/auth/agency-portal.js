267635aeefda57928a46bfbf8faa0d7f20554cd6
Brian Pullin <brian@pullindev.com>
2020-09-15 13:32:26 -0700

    Wholesale: Only Digalent agencyNetwork does the DocuSign process

--- public/v1/auth/agency-portal.js
+++ public/v1/auth/agency-portal.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const serverHelper = require('../../../server.js');
 // eslint-disable-next-line no-unused-vars
 const tracker = global.requireShared('./helpers/tracker.js');
+const AgencyPortalUserBO = global.requireShared('./models/AgencyPortalUser-BO.js');
 
 /**
  * Responds to get requests for an authorization token
@@ -20,34 +21,34 @@ const tracker = global.requireShared('./helpers/tracker.js');
  * @returns {object} res - Returns an authorization token
  */
 async function createToken(req, res, next){
-	let error = false;
+    let error = false;
 
-	// Check for data
-	if (!req.body || typeof req.body !== 'object' || Object.keys(req.body).length === 0) {
-		log.info('Bad Request: Missing both email and password');
-		return next(serverHelper.requestError('You must supply an email address and password'));
-	}
+    // Check for data
+    if (!req.body || typeof req.body !== 'object' || Object.keys(req.body).length === 0) {
+        log.info('Bad Request: Missing both email and password');
+        return next(serverHelper.requestError('You must supply an email address and password'));
+    }
 
-	// Make sure an email was provided
-	if (!req.body.email) {
-		log.info('Missing email' + __location);
-		res.send(400, serverHelper.requestError('Email address is required'));
-		return next();
-	}
+    // Make sure an email was provided
+    if (!req.body.email) {
+        log.info('Missing email' + __location);
+        res.send(400, serverHelper.requestError('Email address is required'));
+        return next();
+    }
 
-	// Makes sure a password was provided
-	if (!req.body.password) {
-		log.info('Missing password' + __location);
-		res.send(400, serverHelper.requestError('Password is required'));
-		return next();
-	}
+    // Makes sure a password was provided
+    if (!req.body.password) {
+        log.info('Missing password' + __location);
+        res.send(400, serverHelper.requestError('Password is required'));
+        return next();
+    }
 
-	// This is a complete hack. Plus signs in email addresses are valid, but the Restify queryParser removes plus signs. Add them back in
-	req.body.email = req.body.email.replace(' ', '+');
+    // This is a complete hack. Plus signs in email addresses are valid, but the Restify queryParser removes plus signs. Add them back in
+    req.body.email = req.body.email.replace(' ', '+');
 
-	// Authenticate the information provided by the user
-	const emailHash = await crypt.hash(req.body.email);
-	const agencySQL = `
+    // Authenticate the information provided by the user
+    const emailHash = await crypt.hash(req.body.email);
+    const agencySQL = `
 		SELECT
 			\`apu\`.\`agency_network\`,
 			\`apu\`.\`agency\`,
@@ -64,90 +65,91 @@ async function createToken(req, res, next){
 		WHERE \`apu\`.\`email_hash\` = ${db.escape(emailHash)} AND \`apu\`.\`state\` > 0
 		LIMIT 1;
 	`;
-	const agencyPortalUserResult = await db.query(agencySQL).catch(function(e) {
-		log.error(e.message + __location);
-		res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
-		error = true;
-	});
-	if (error) {
-		return next(false);
-	}
+    const agencyPortalUserResult = await db.query(agencySQL).catch(function(e) {
+        log.error(e.message + __location);
+        res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
+        error = true;
+    });
+    if (error) {
+        return next(false);
+    }
 
-	// Make sure we found the user
-	if (!agencyPortalUserResult || !agencyPortalUserResult.length) {
-		log.info('Authentication failed - Account not found');
-		log.verbose(emailHash);
-		res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
-		return next();
-	}
+    // Make sure we found the user
+    if (!agencyPortalUserResult || !agencyPortalUserResult.length) {
+        log.info('Authentication failed - Account not found');
+        log.verbose(emailHash);
+        res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
+        return next();
+    }
 
-	// Check the password
-	if (!await crypt.verifyPassword(agencyPortalUserResult[0].password, req.body.password)) {
-		log.info('Authentication failed - Bad password');
-		res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
-		return next();
-	}
+    // Check the password
+    if (!await crypt.verifyPassword(agencyPortalUserResult[0].password, req.body.password)) {
+        log.info('Authentication failed - Bad password');
+        res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
+        return next();
+    }
 
-	// Begin constructing the payload
-	const payload = {
-		agencyNetwork: false,
-		agents: [],
-		signatureRequired: false
-	};
+    // Begin constructing the payload
+    const payload = {
+        agencyNetwork: false,
+        agents: [],
+        signatureRequired: false
+    };
 
-	// Record the time this user logged in
-	const lastLoginSQL = `
+    // Record the time this user logged in
+    const lastLoginSQL = `
 		UPDATE \`#__agency_portal_users\`
 		SET \`last_login\` = NOW()
 		WHERE \`id\` = ${agencyPortalUserResult[0].id}
 		LIMIT 1;
 	`;
-	db.query(lastLoginSQL).catch(function(e) {
-		// If this fails, log the failure but do nothing else
-		log.error(e.message);
-	});
+    db.query(lastLoginSQL).catch(function(e) {
+        // If this fails, log the failure but do nothing else
+        log.error(e.message);
+    });
 
-	// Check if this was an agency network
-	if (agencyPortalUserResult[0].agency_network) {
+    // Check if this was an agency network
+    if (agencyPortalUserResult[0].agency_network) {
         payload.agencyNetwork = agencyPortalUserResult[0].agency_network;
         //agency network ID now in payload for consistency between network and agency.
         payload.agencyNetworkId = agencyPortalUserResult[0].agency_network;
-	}
+    }
 
-	// Store a local copy of the agency network ID .
+    // Store a local copy of the agency network ID .
     let agencyNetworkId = payload.agencyNetwork;
 
-	// For agency networks get the agencies they are allowed to access
-	if (payload.agencyNetwork) {
-		// Build and execute the query
-		const agenciesSQL = `
+    // For agency networks get the agencies they are allowed to access
+    if (payload.agencyNetwork) {
+        // Build and execute the query
+        const agenciesSQL = `
 			SELECT \`id\`
 			FROM \`#__agencies\`
 			WHERE \`agency_network\` = ${db.escape(payload.agencyNetwork)} AND \`state\` > 0;
 		`;
-		const agencies = await db.query(agenciesSQL).catch(function(e) {
-			log.error(e.message + __location);
-			res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
-			error = true;
-		});
-		if (error) {
-			return next(false);
-		}
+        const agencies = await db.query(agenciesSQL).catch(function(e) {
+            log.error(e.message + __location);
+            res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
+            error = true;
+        });
+        if (error) {
+            return next(false);
+        }
 
-		// Store the agencies in the payload
-		payload.agents = [];
-		agencies.forEach((agency) => {
-			payload.agents.push(agency.id);
-		});
-	}else{
-		// Just allow access to the current agency
-		payload.agents.push(agencyPortalUserResult[0].agency);
+        // Store the agencies in the payload
+        payload.agents = [];
+        agencies.forEach((agency) => {
+            payload.agents.push(agency.id);
+        });
+    }
+    else{
+        // Just allow access to the current agency
+        payload.agents.push(agencyPortalUserResult[0].agency);
 
-		// Add the signing authority permission to the payload
-		payload.canSign = Boolean(agencyPortalUserResult[0].can_sign);
+        // Add the signing authority permission to the payload
+        payload.canSign = Boolean(agencyPortalUserResult[0].can_sign);
 
-		// Determine whether or not the user needs to sign a wholesale agreement
-		const wholesaleSQL = `
+        // Determine whether or not the user needs to sign a wholesale agreement
+        const wholesaleSQL = `
 			SELECT
 				\`agency_network\`,
 				\`wholesale\` ,
@@ -156,71 +158,83 @@ async function createToken(req, res, next){
 			WHERE \`id\` = ${db.escape(agencyPortalUserResult[0].agency)} AND \`state\` > 0
 			LIMIT 1;
 		`;
-		const wholesaleInfo = await db.query(wholesaleSQL).catch(function(e) {
-			log.error(e.message + __location);
-			res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
-			error = true;
-		});
-		if (error) {
-			return next(false);
-		}
+        const wholesaleInfo = await db.query(wholesaleSQL).catch(function(e) {
+            log.error(e.message + __location);
+            res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
+            error = true;
+        });
+        if (error) {
+            return next(false);
+        }
 
-		// Only agencies who have wholesale enabled and have not signed before should be required to sign
-		if (wholesaleInfo[0].wholesale && !wholesaleInfo[0].wholesale_agreement_signed) {
-			payload.signatureRequired = true;
-		}
+        // Only agencies who have wholesale enabled and have not signed before should be required to sign
+        // Only for for Digalent is the process handle in the software.
+        if (wholesaleInfo[0].wholesale && !wholesaleInfo[0].wholesale_agreement_signed && wholesaleInfo[0].agency_network === 2) {
+            payload.signatureRequired = true;
+        }
+        else if (wholesaleInfo[0].wholesale && !wholesaleInfo[0].wholesale_agreement_signed && wholesaleInfo[0].agency_network === 1){
+            // mark them as signed ??
+            try{
+                const agencyPortalUserBO = new AgencyPortalUserBO();
+                await agencyPortalUserBO.markWholeSaleSignedById(agencyPortalUserResult[0].agency)
+            }
+            catch(err){
+                // do not stop login over this error.
+                log.error("Error marking wheelhouse user wholesale " + err + __location);
+            }
+        }
 
-		// Store the agency network ID locally for later use
-		agencyNetworkId = wholesaleInfo[0].agency_network;
-	}
+        // Store the agency network ID locally for later use
+        agencyNetworkId = wholesaleInfo[0].agency_network;
+    }
 
-	// Build a query to get all of the insurers this agency network can use
-	const insurersSQL = `
+    // Build a query to get all of the insurers this agency network can use
+    const insurersSQL = `
 		SELECT \`i\`.\`id\`
 		FROM \`#__insurers\` AS \`i\`
 		RIGHT JOIN \`#__agency_network_insurers\` AS \`ani\` ON \`i\`.\`id\` = \`ani\`.\`insurer\`
 		WHERE \`ani\`.\`agency_network\` = ${db.escape(agencyNetworkId)} AND \`i\`.\`state\` > 0;
 	`;
 
-	// Query the database
-	const insurersData = await db.query(insurersSQL).catch(function(e){
-		log.error(e.message);
-		res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
-		error = true;
-	});
-	if(error){
-		return next(false);
-	}
+    // Query the database
+    const insurersData = await db.query(insurersSQL).catch(function(e){
+        log.error(e.message);
+        res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
+        error = true;
+    });
+    if(error){
+        return next(false);
+    }
 
-	// Store the insurers in the payload
-	payload.insurers = [];
-	insurersData.forEach((insurer) => {
-		payload.insurers.push(insurer.id);
-	});
+    // Store the insurers in the payload
+    payload.insurers = [];
+    insurersData.forEach((insurer) => {
+        payload.insurers.push(insurer.id);
+    });
 
-	// Add the user ID to the payload
+    // Add the user ID to the payload
     payload.userID = agencyPortalUserResult[0].id;
     payload.agencyNetworkId = agencyNetworkId;
 
-	// Add the permissions to the payload
-	payload.permissions = JSON.parse(agencyPortalUserResult[0].permissions);
+    // Add the permissions to the payload
+    payload.permissions = JSON.parse(agencyPortalUserResult[0].permissions);
 
-	// Check whether or not this is the first time the user is logging in
-	payload.firstLogin = Boolean(agencyPortalUserResult[0].last_login);
+    // Check whether or not this is the first time the user is logging in
+    payload.firstLogin = Boolean(agencyPortalUserResult[0].last_login);
 
-	// Report back whether or not a password reset is required
-	payload.resetRequired = Boolean(agencyPortalUserResult[0].reset_required);
+    // Report back whether or not a password reset is required
+    payload.resetRequired = Boolean(agencyPortalUserResult[0].reset_required);
 
-	// Return the version of the Terms of Service
-	payload.termsOfServiceVersion = agencyPortalUserResult[0].termsOfServiceVersion;
+    // Return the version of the Terms of Service
+    payload.termsOfServiceVersion = agencyPortalUserResult[0].termsOfServiceVersion;
 
-	// This is a valid user, generate and return a token
-	const token = `Bearer ${jwt.sign(payload, global.settings.AUTH_SECRET_KEY, {expiresIn: global.settings.JWT_TOKEN_EXPIRATION})}`;
-	res.send(201, {
-		status: 'Created',
-		token: token
-	});
-	return next();
+    // This is a valid user, generate and return a token
+    const token = `Bearer ${jwt.sign(payload, global.settings.AUTH_SECRET_KEY, {expiresIn: global.settings.JWT_TOKEN_EXPIRATION})}`;
+    res.send(201, {
+        status: 'Created',
+        token: token
+    });
+    return next();
 }
 
 /**
@@ -233,39 +247,39 @@ async function createToken(req, res, next){
  * @returns {object} Returns an updated authorization token
  */
 async function updateToken(req, res, next) {
-	// Ensure we have the proper parameters
-	if (!req.body || !req.body.token) {
-		log.info('Bad Request: Missing "token" parameter when trying to refresh the token ' + __location);
-		return next(serverHelper.requestError('A parameter is missing when trying to refresh the token.'));
-	}
+    // Ensure we have the proper parameters
+    if (!req.body || !req.body.token) {
+        log.info('Bad Request: Missing "token" parameter when trying to refresh the token ' + __location);
+        return next(serverHelper.requestError('A parameter is missing when trying to refresh the token.'));
+    }
 
-	// Ensure it is a valid JWT and that it hasn't expired.
-	let token = null;
-	try {
-		token = jwt.verify(req.body.token, global.settings.AUTH_SECRET_KEY);
-	}
-	catch (error) {
-		log.error("JWT: " + error + __location);
-		return next(serverHelper.forbiddenError('Invalid token'));
-	}
+    // Ensure it is a valid JWT and that it hasn't expired.
+    let token = null;
+    try {
+        token = jwt.verify(req.body.token, global.settings.AUTH_SECRET_KEY);
+    }
+    catch (error) {
+        log.error("JWT: " + error + __location);
+        return next(serverHelper.forbiddenError('Invalid token'));
+    }
 
-	// Valid JWT. Preserve the content except for the issued at and expiration timestamps
-	delete token.iat;
-	delete token.exp;
+    // Valid JWT. Preserve the content except for the issued at and expiration timestamps
+    delete token.iat;
+    delete token.exp;
 
-	// Sign the JWT with new timestamps
-	token = `Bearer ${jwt.sign(token, global.settings.AUTH_SECRET_KEY, {expiresIn: global.settings.JWT_TOKEN_EXPIRATION})}`;
+    // Sign the JWT with new timestamps
+    token = `Bearer ${jwt.sign(token, global.settings.AUTH_SECRET_KEY, {expiresIn: global.settings.JWT_TOKEN_EXPIRATION})}`;
 
-	// Send it back
-	res.send(201, {
-		status: 'Created',
-		token: token
-	});
-	return next();
+    // Send it back
+    res.send(201, {
+        status: 'Created',
+        token: token
+    });
+    return next();
 }
 
 /* -----==== Endpoints ====-----*/
 exports.registerEndpoint = (server, basePath) => {
-	server.addPost('Create Token', `${basePath}/agency-portal`, createToken);
-	server.addPut('Refresh Token', `${basePath}/agency-portal`, updateToken);
+    server.addPost('Create Token', `${basePath}/agency-portal`, createToken);
+    server.addPut('Refresh Token', `${basePath}/agency-portal`, updateToken);
 };
