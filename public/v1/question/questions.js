9ec715dc369b067d48d62a5a4368c2c4a40631f2
Johnny Belcher <johnny@talageins.com>
2020-10-02 10:43:12 -0700

    allow insurers to be default

--- public/v1/question/questions.js
+++ public/v1/question/questions.js
@@ -61,7 +61,9 @@ async function getQuestions(req, res, next){
 
     let getQuestionsResult = null;
     try{
-        getQuestionsResult = await questionSvc.GetQuestionsForFrontend(req.query.activity_codes.split(','), req.query.industry_code, req.query.zips.split(','), req.query.policy_types.split(','), req.query.insurers.split(','), return_hidden);
+        // insurers is optional
+        const insurers = req.query.insurers ? req.query.insurers.split(',') : [];
+        getQuestionsResult = await questionSvc.GetQuestionsForFrontend(req.query.activity_codes.split(','), req.query.industry_code, req.query.zips.split(','), req.query.policy_types.split(','), insurers, return_hidden);
     }
     catch(error){
         return next(serverHelper.requestError('An error occured while retrieving application questions.'));
