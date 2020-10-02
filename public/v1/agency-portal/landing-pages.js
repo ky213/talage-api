c5a8596d485003e54b125a30261fea90886e65ac
Raja Singh <raja@talageins.com>
2020-10-02 16:25:51 -0700

    added functionality to take agency id as part of request from agency network and return list of landing pages for given agency.

--- public/v1/agency-portal/landing-pages.js
+++ public/v1/agency-portal/landing-pages.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const auth = require('./helpers/auth.js');
+const validator = global.requireShared('./helpers/validator.js');
 const serverHelper = require('../../../server.js');
 
 /**
@@ -12,19 +13,50 @@ const serverHelper = require('../../../server.js');
  * @returns {void}
  */
 async function getLandingPages(req, res, next){
-	// TO DO: Add support for Agency Networks (take in an angency as a parameter)
-	const agency = req.authentication.agents[0];
+	let error = false;
+	let where = ``;
+	if(req.authentication.agencyNetwork && req.query.agency){
+		// Get all agencies belonging to this agency network
+		const agencies = await auth.getAgents(req).catch(function(e){error = e;})
+		if(error){
+			return next(error);
+		} 
+		// validate the agency id provided is valid
+		if (!await validator.agent(req.query.agency)) {
+			log.warn(`Agency validation error: ${__location}`)
+			return next(serverHelper.notFoundError('Agency is invalid'));
+		}
+		// Make sure the current agency network has the agency as part of its network
+		if (!agencies.includes(parseInt(req.query.agency, 10))) {
+			log.warn(`Agency network tried to access information about an agency that is not part of its network. ${__location}`);
+			return next(serverHelper.notFoundError('Agency is invalid'));
+		}
+		where = `AND \`alp\`.\`agency\` = ${parseInt(req.query.agency, 10)}`;
+	}
+	else if (req.authentication.agencyNetwork){
+		where = `AND \`alp\`.\`agency_network\`= ${parseInt(req.authentication.agencyNetwork, 10)}`;
+	}
+	else {
+		// Get the agents that we are permitted to view
+		const agents = await auth.getAgents(req).catch(function(e){
+			error = e;
+		});
+		if (error){
+			return next(error);
+		}
 
+		where = `AND \`alp\`.\`agency\` = ${parseInt(agents[0], 10)}`;
+	}
 	// Build a query that will return all of the landing pages
 	const landingPageSQL = `
 			SELECT
-				\`id\`,
-				\`hits\`,
-				\`name\`,
-				\`slug\`,
-				\`primary\`
-			FROM \`#__agency_landing_pages\`
-			WHERE \`agency\` = ${parseInt(agency, 10)} AND \`state\` > 0;
+			\`alp\`.\`id\`,
+			\`alp\`.\`hits\`,
+			\`alp\`.\`name\`,
+			\`alp\`.\`slug\`,
+			\`alp\`.\`primary\`
+			FROM \`#__agency_landing_pages\` as \`alp\`
+			WHERE \`state\` > 0 ${where};
 		`;
 
 	// Run the query
