a06fe80ca05e830dfafd31ce9dcee8a38c777151
Scott Fritzinger <scott@talageins.com>
2020-10-14 18:25:37 -0700

    Fixing issue where their API fails and does not return any error description

--- public/v1/quote/helpers/integrations/compwest/wc.js
+++ public/v1/quote/helpers/integrations/compwest/wc.js
@@ -653,11 +653,15 @@ module.exports = class CompwestWC extends Integration {
 
         // Check the status to determine what to do next
         let message_type = '';
-        const status = res.SignonRs[0].Status[0].StatusCd[0];
+        const status = this.get_xml_child(result.ACORD, "SignonRs.Status.StatusCd");
+        let statusDescription = this.get_xml_child(result.ACORD, "SignonRs.Status.StatusDesc.Desc");
+        if (!statusDescription) {
+            statusDescription = "AFGroup/CompWest did not return an error description.";
+        }
         switch (status) {
             case 'DECLINE':
                 this.log += `--------======= Application Declined =======--------<br><br>Appid: ${this.app.id}  ${this.insurer.name} declined to write this business`;
-                this.reasons.push(`${status} - ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}`);
+                this.reasons.push(`${status} - ${statusDescription}`);
                 return this.return_result(status);
             case 'UNAUTHENTICATED':
             case 'UNAUTHORIZED':
@@ -668,20 +672,20 @@ module.exports = class CompwestWC extends Integration {
                 this.reasons.push(`${status} - ${message_type} Agency ID`);
                 return this.return_result('error');
             case 'ERRORED':
-                    this.log += `--------======= Application error =======--------<br><br> ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}`;
-                    log.error(`Appid: ${this.app.id}  ${this.insurer.name} ${this.policy.type} Integration Error(s):\n--- ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}` + __location);
-                    this.reasons.push(`${status} - ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}`);
-                    // Send notification email if we get an E Mod error back from carrier
-                    if (res.SignonRs[0].Status[0].StatusDesc[0].Desc[0].toLowerCase().includes("experience mod")) {
-                        wcEmodEmail.sendEmodEmail(this.app.id);
-                    }
-                    return this.return_result('error');
-            case 'SMARTEDITS':
-                this.log += `--------======= Application SMARTEDITS =======--------<br><br>${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}`;
-                log.info(`Appid: ${this.app.id} ${this.insurer.name} ${this.policy.type} Integration Carrier returned SMARTEDITS :\n--- ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}` + __location);
-                this.reasons.push(`${status} - ${res.SignonRs[0].Status[0].StatusDesc[0].Desc[0]}`);
+                this.log += `--------======= Application error =======--------<br><br> ${statusDescription}`;
+                log.error(`Appid: ${this.app.id}  ${this.insurer.name} ${this.policy.type} Integration Error(s):\n--- ${statusDescription}` + __location);
+                this.reasons.push(`${status} - ${statusDescription}`);
                 // Send notification email if we get an E Mod error back from carrier
-                if (res.SignonRs[0].Status[0].StatusDesc[0].Desc[0].toLowerCase().includes("experience mod")) {
+                if (statusDescription.toLowerCase().includes("experience mod")) {
+                    wcEmodEmail.sendEmodEmail(this.app.id);
+                }
+                return this.return_result('error');
+            case 'SMARTEDITS':
+                this.log += `--------======= Application SMARTEDITS =======--------<br><br>${statusDescription}`;
+                log.info(`Appid: ${this.app.id} ${this.insurer.name} ${this.policy.type} Integration Carrier returned SMARTEDITS :\n--- ${statusDescription}` + __location);
+                this.reasons.push(`${status} - ${statusDescription}`);
+                // Send notification email if we get an E Mod error back from carrier
+                if (statusDescription.toLowerCase().includes("experience mod")) {
                     wcEmodEmail.sendEmodEmail(this.app.id);
                 }
                 return this.return_result('referred');
