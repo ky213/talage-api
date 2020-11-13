ead0b0d9687249f2c967ffc7ed5194c55d9ed331
Brian Pullin <brian@talageins.com>
2020-11-13 14:42:55 -0800

    Auth/AP   removed use of clw_talage_agency_portal_user_groups

--- public/v1/agency-portal/user-groups.js
+++ public/v1/agency-portal/user-groups.js
@@ -14,27 +14,13 @@ const AgencyPortalUserBO = global.requireShared('models/AgencyPortalUser-BO.js')
  * @returns {void}
  */
 async function getUserGroups(req, res, next){
-    // // Get the user groups, excluding 'Administrator' for now as this permission is currently unused. It will be added soon.
-    // const userGroupsSQL = `
-    // 		SELECT
-    // 			\`id\`,
-    // 			\`name\`,
-    // 			\`permissions\`
-    // 		FROM \`#__agency_portal_user_groups\`
-    // 		WHERE \`id\` != 3 and name != 'Talage Super User';
-    // 	`;
-    // //TODO Fix Catch logic
-    // const userGroups = await db.query(userGroupsSQL).catch(function(err){
-    //     log.error('__agency_portal_user_groups error ' + err + __location);
-    // 	return next(serverHelper.internalError('Well, that wasn\’t supposed to happen, but hang on, we\’ll get it figured out quickly and be in touch.'));
-    // });
     let userGroups = null;
     try{
         const agencyPortalUserBO = new AgencyPortalUserBO();
         userGroups = await agencyPortalUserBO.getGroupList();
     }
     catch(err){
-        log.error('__agency_portal_user_groups error ' + err + __location);
+        log.error('agencyPortalUserBO error ' + err + __location);
         return next(serverHelper.internalError('Well, that wasn\’t supposed to happen, but hang on, we\’ll get it figured out quickly and be in touch.'));
     }
 
