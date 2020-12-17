0f89a69dd364de6306a73d3f46be6fde3b535231
Natalie Arnold <natalie@talageins.com>
2020-12-17 12:15:17 -0800

    the horror has been fixed through witchcraft and luck

--- shared/services/acord/pdf-helper.js
+++ shared/services/acord/pdf-helper.js
@@ -2,56 +2,59 @@ const fs = require('fs');
 const { degrees, PDFDocument, rgb, StandardFonts } = require('pdf-lib');
 const path = require('path');
 
-const createPDF = async (sourcePDFString, dataFieldsObj) => {
+const createPDF = async(sourcePDFString, dataFieldsObj) => {
     const pathToPdfString = path.resolve(__dirname, './pdf/' + sourcePDFString);
 
     try {
-        const pdfPage = await PDFDocument.load(fs.readFileSync(pathToPdfString));
+        const pdfPage = await PDFDocument.load(fs.readFileSync(pathToPdfString).buffer);
         const form = pdfPage.getForm();
 
-        for (const i of Object.keys(dataFieldsObj)) {
-            if (!dataFieldsObj[i]) {
+        for (const formFieldString of Object.keys(dataFieldsObj)) {
+            if (!dataFieldsObj[formFieldString]) {
                 continue;
             }
-            const field = form.getField(i);
+            const field = form.getField(formFieldString);
             switch (field.constructor.name) {
                 case 'PDFTextField':
-                    field.setText(dataFieldsObj[i].toString());
+                    field.setText(dataFieldsObj[formFieldString].toString());
                     break;
-                
+
                 case 'PDFCheckBox':
-                    if (dataFieldsObj[i]) {
+                    if (dataFieldsObj[formFieldString]) {
                         field.check()
-                    } else {
+                    }
+                    else {
                         field.uncheck();
                     }
                     break;
-                
+
                 default:
                     throw new Error(`Unknown type: ${field.constructor.name}`);
-            }            
+            }
         }
-        return pdfPage;
+
+        return pdfPage.save();
     }
     catch(err){
         log.error('Failed to generate PDF ' + err + __location);
         throw err;
     }
-    return null;
+
 }
 
-const createMultiPagePDF = async (pdfList) => {
+const createMultiPagePDF = async(pdfList) => {
     const mergedPdf = await PDFDocument.create();
 
     for (const pdf of pdfList) {
         if (!pdf) {
             throw new Error("Invalid PDF file passed in")
         }
-        const copiedPages = await mergedPdf.copyPages(pdf, pdf.getPageIndices());
+        const test = await PDFDocument.load(pdf);
+        const copiedPages = await mergedPdf.copyPages(test, test.getPageIndices());
         copiedPages.forEach((page) => mergedPdf.addPage(page));
     }
 
-    return mergedPdf;
+    return mergedPdf.save();
 }
 
 module.exports = {
