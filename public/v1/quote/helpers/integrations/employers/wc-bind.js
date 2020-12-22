a6b8577888635d3dc8946141a193fba9198fb6b7
Roger Keller <rkeller@talageins.com>
2020-12-22 10:20:33 -0800

    Adding note about using curly with Employers

--- public/v1/quote/helpers/integrations/employers/wc-bind.js
+++ public/v1/quote/helpers/integrations/employers/wc-bind.js
@@ -1,4 +1,12 @@
 const Bind = require('../bind');
+/**
+ * NOTE: Need to use node-libcurl for Employers because they are sending
+ * non-compliant HTTP headers. And node-libcurl handles this better due to
+ * using curl versus the node HTTP library when parsing HTTP responses.
+ * 
+ * Hopefully at some point Employers will update their code and we can
+ * move this to use Axios.
+ */
 const { curly } = require('node-libcurl');
 
 class EmployersBind extends Bind {
@@ -11,6 +19,7 @@ class EmployersBind extends Bind {
             ]
         }
         let host = this.insurer.useSandbox ? 'api-qa.employers.com' : 'api.employers.com';
+        /** See Note above about using curly for Employers. */
         const result = await curly.put(`https://${host}/DigitalAgencyServices/quote/${this.quote.requestId}/bind`, headers);
         if (!result.data.success) {
             throw new Error(JSON.stringify(result.data.errors));
