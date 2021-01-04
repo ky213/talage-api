eb8f7a6bbecc4627d992da63b5d540a88f097831
Natalie Arnold <natalie@talageins.com>
2021-01-04 13:34:57 -0800

    blank bop pdf

--- shared/services/acord/policies/bop.js
+++ shared/services/acord/policies/bop.js
@@ -1,7 +1,21 @@
 'use strict'
 
 const ACORD = require("../acord.js");
+const PdfHelper = require('../pdf-helper');
 
 module.exports = class AcordBOP extends ACORD{
+    async create(){
 
+        await this.dataInit('BOP');
+
+        const pdfList = [];
+
+        pdfList.push(await this.createAcord125());
+        pdfList.push(await this.createAcord140());
+        pdfList.push(await this.createQuestionsTable());
+
+        const pdf = PdfHelper.createMultiPagePDF(pdfList);
+
+        return pdf;
+    }
 }
