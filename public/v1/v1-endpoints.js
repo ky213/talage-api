10e0696317de2e3db5caaf9960edafe9fd9ab234
Brian Pullin <brian@talageins.com>
2021-01-22 10:16:32 -0800

    API endpoint update

--- public/v1/v1-endpoints.js
+++ public/v1/v1-endpoints.js
@@ -152,10 +152,10 @@ exports.registerEndpoints = (server) => {
     registerEndpoint(server, 'administration', 'mapping-rt');
 
     if(global.settings.ENABLE_GENERIC_API === "YES"){
-        registerEndpoint(server, 'api/code', 'industry-categories');
-        registerEndpoint(server, 'api/code', 'industry-codes');
-        registerEndpoint(server, 'api', 'agency');
-        registerEndpoint(server, 'api', 'application');
+        registerEndpoint(server, 'api/code', 'industry-categories-rt');
+        registerEndpoint(server, 'api/code', 'industry-codes-rt');
+        registerEndpoint(server, 'api', 'agency-rt');
+        registerEndpoint(server, 'api', 'application-rt');
     }
 
     // Server.AddGet('Uptime Check', '/', GetUptime);
