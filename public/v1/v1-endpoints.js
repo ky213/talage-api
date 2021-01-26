778ba3b0499153dfff39a334119f66eedd75f263
Johnny Belcher <johnny@talageins.com>
2021-01-26 11:43:06 -0800

    remove code from path for industry/categories

--- public/v1/v1-endpoints.js
+++ public/v1/v1-endpoints.js
@@ -152,8 +152,8 @@ exports.registerEndpoints = (server) => {
     registerEndpoint(server, 'administration', 'mapping-rt');
 
     if(global.settings.ENABLE_GENERIC_API === "YES"){
-        registerEndpoint(server, 'api/code', 'industry-categories-rt');
-        registerEndpoint(server, 'api/code', 'industry-codes-rt');
+        registerEndpoint(server, 'api', 'industry-categories-rt');
+        registerEndpoint(server, 'api', 'industry-codes-rt');
         registerEndpoint(server, 'api', 'agency-rt');
         registerEndpoint(server, 'api', 'application-rt');
     }
