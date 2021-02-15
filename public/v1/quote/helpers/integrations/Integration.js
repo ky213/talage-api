3372daa0e38e82100fef876d086fa50f36523f65
Brian Pullin <brian@talageins.com>
2021-02-15 14:01:14 -0800

    Integration.js IndustryCode and territory no match lowered log message to warning.

--- public/v1/quote/helpers/integrations/Integration.js
+++ public/v1/quote/helpers/integrations/Integration.js
@@ -2237,8 +2237,8 @@ module.exports = class Integration {
             }
             if (!result || !result.length) {
                 if (this.requiresInsurerIndustryCodes) {
-                    this.reasons.push("An insurer industry class code was not found for the given industry.");
-                    log.error(`AppId: ${this.app.id} InsurerId: ${this.insurer.id} _insurer_supports_industry_codes required insurer mapping for this industry code was not found. query=${sql} ` + __location);
+                    this.reasons.push("An insurer industry class code was not found for the given industry and territory.");
+                    log.warn(`AppId: ${this.app.id} InsurerId: ${this.insurer.id} _insurer_supports_industry_codes required insurer mapping for this industry code was not found. query=${sql} ` + __location);
                     fulfill(false);
                     return;
                 }
