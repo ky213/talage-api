f3e41abd901f0e6cf3b508aeaac5fc29051cbcbc
Brian Pullin <brian@talageins.com>
2021-02-19 15:07:41 -0800

    API GetToken agency portal user access

--- public/v1/api/auth-api-rt.js
+++ public/v1/api/auth-api-rt.js
@@ -1,6 +1,7 @@
 const serverHelper = global.requireRootPath('server.js');
 const {'v4': uuidv4} = require('uuid');
 const moment = require('moment');
+const crypt = global.requireShared('./services/crypt.js');
 
 /**
  * Responds to get requests for an API authorization token
@@ -12,46 +13,49 @@ const moment = require('moment');
  * @returns {object} res - Returns an authorization token
  */
 async function getToken(req, res, next) {
-    //let error = false;
+    let error = false;
 
     // Query parameters user, key, and joomla_user are all optional now since the quoting engine was broken out
     const payload = {};
 
-    // Joolma not supported anymore.
-    if (req.query.joomla_user) {
-        res.send(403, serverHelper.forbiddenError('forbidden user'));
-        return next();
-    }
     // TOOD basic Auth for apikey and apiSecret.
 
-    // // Validate the passed-in user and key
-    // if (req.query.user && req.query.key) {
-    //     // Authenticate the information provided by the user
-    //     const sql = `SELECT \`id\`, \`key\` FROM \`#__api_users\` WHERE \`user\` = ${db.escape(req.query.user)} LIMIT 1;`;
-    //     const result = await db.query(sql).catch(function(e) {
-    //         log.error(e.message);
-    //         res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
-    //         error = true;
-    //     });
-    //     if (error) {
-    //         return next(false);
-    //     }
+    //check for Agency Portal user access
+    // Validate the passed-in user and key
+    if (req.body.user && req.body.password) {
+        // Authenticate the information provided by the user
+        const sql = `SELECT id, password FROM clw_talage_agency_portal_users WHERE clear_email = ${db.escape(req.body.user)} LIMIT 1;`;
+        const result = await db.query(sql).catch(function(e) {
+            log.error(e.message);
+            res.send(500, serverHelper.internalError('Error querying database. Check logs.'));
+            error = true;
+        });
+        if (error) {
+            return next(false);
+        }
 
-    //     // Check that the key was valid
-    //     if (!result.length) {
-    //         log.info('Authentication failed');
-    //         res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
-    //         return next();
-    //     }
+        // Check that the key was valid
+        if (!result.length) {
+            log.info('Authentication failed - No DB record' + __location);
+            res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
+            return next();
+        }
 
-    //     // Check the key
-    //     if (!await crypt.verifyPassword(result[0].key, req.query.key)) {
-    //         log.info('Authentication failed');
-    //         res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
-    //         return next();
-    //     }
-    //     payload.api_id = result[0].id;
-    // }
+        // Check the key
+        if (!await crypt.verifyPassword(result[0].password, req.body.password)) {
+            log.info('Authentication failed - No Password chck' + __location);
+            res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
+            return next();
+        }
+        // TODO check API access and Application manage rights
+
+        payload.userId = result[0].id;
+    }
+    else {
+        log.info('Authentication failed - Bad Request ' + __location);
+        res.send(401, serverHelper.invalidCredentialsError('Invalid API Credentials'));
+        return next();
+    }
 
     payload.apiToken = true;
     payload.ranValue1 = uuidv4().toString();
@@ -173,4 +177,5 @@ exports.AddApplicationToToken = async function(req, applicationId) {
 /* -----==== Endpoints ====-----*/
 exports.registerEndpoint = (server, basePath) => {
     server.addGet('Get Token', `${basePath}/auth`, getToken);
+    server.addPut('PUT getToken', `${basePath}/auth`, getToken);
 };
