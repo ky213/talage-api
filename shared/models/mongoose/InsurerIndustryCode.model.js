21cba9b411af265c06da59fc3462b727288efda7
Brian Pullin <brian@talageins.com>
2021-04-27 19:07:15 -0700

    InsurerIndustryCode removal from model.

--- shared/models/mongoose/InsurerIndustryCode.model.js
+++ shared/models/mongoose/InsurerIndustryCode.model.js
@@ -43,11 +43,6 @@ const InsurerIndustryCodeSchema = new Schema({
     active: {type: Boolean, default: true}
 },opts)
 
-
-InsurerIndustryCodeSchema.index({insurerId: 1}); // Insure Index
-//InsurerIndustryCodeSchema.index({insurerId: 1, policyType: 1, code: 1});
-//InsurerIndustryCodeSchema.index({insurerId: 1, talageIndustryCodeIdList: 1});
-
 // //***** Virtuals old field names ****************** */
 
 InsurerIndustryCodeSchema.plugin(timestamps);
