f87b18282732ab23afed6f7c0c68cd7e181fe0e0
Brian Pullin <brian@talageins.com>
2021-05-02 16:27:34 -0700

    InsurerActivity Code - no talage activity and no insurer question filters.

--- shared/models/InsurerIndustryCode-BO.js
+++ shared/models/InsurerIndustryCode-BO.js
@@ -194,7 +194,7 @@ module.exports = class InsurerIndustryCodeBO{
                 return;
             }
             if(docList && docList.length > 0){
-                // pass back the count as well for api paging (so we know how many total rows are)
+                // BAD - BREAK PATTERN TODO REVERT BACK TO PATTERN of the other BOs pass back the count as well for api paging (so we know how many total rows are)
                 if (findCount){
                     resolve({
                         rows: mongoUtils.objListCleanup(docList),
