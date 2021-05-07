9fc5b378a1fea0f740d5e7436a7ed6981c422107
Raja Singh <raja@talageins.com>
2021-05-07 13:53:14 -0700

    remove check for id greater than zero since string not an integer

--- shared/models/Application-BO.js
+++ shared/models/Application-BO.js
@@ -2202,18 +2202,20 @@ module.exports = class ApplicationModel {
     deleteSoftById(id) {
         return new Promise(async(resolve, reject) => {
             //validate
-            if (id && id > 0) {
-
-                //Remove old records.
-                const sql = `Update ${collectionName} 
-                        SET state = -2
-                        WHERE id = ${db.escape(id)}
-                `;
-                //let rejected = false;
-                await db.query(sql).catch(function(err) {
-                    // Check if this was
-                    log.error(`Database Object ${collectionName} UPDATE State error : ` + err + __location);
-                });
+            if (id ) {
+                // dont know if we need this code still? but id will only be greater than 1 if it is numerical
+                if(id > 0){
+                    //Remove old records.
+                    const sql = `Update ${collectionName} 
+                            SET state = -2
+                            WHERE id = ${db.escape(id)}
+                    `;
+                    //let rejected = false;
+                    await db.query(sql).catch(function(err) {
+                        // Check if this was
+                        log.error(`Database Object ${collectionName} UPDATE State error : ` + err + __location);
+                    });
+                }
                 // if (rejected) {
                 //     return false;
                 // }
@@ -2225,7 +2227,7 @@ module.exports = class ApplicationModel {
                     await applicationDoc.save();
                 }
                 catch (err) {
-                    log.error("Error get marking Application from mysqlId " + err + __location);
+                    log.error(`Error marking Application from uuid ${id} ` + err + __location);
                     reject(err);
                 }
                 resolve(true);
@@ -2239,7 +2241,7 @@ module.exports = class ApplicationModel {
 
     getAgencyNewtorkIdById(id) {
         return new Promise(async(resolve, reject) => {
-            if(id && id > 0){
+            if(id){
                 let agencyNetworkId = 0;
                 try{
                     const appDoc = await this.loadById(id)
@@ -2258,7 +2260,7 @@ module.exports = class ApplicationModel {
             }
             else {
                 log.error(`getAgencyNewtorkIdById no ID supplied  ${id}` + __location);
-                reject(new Error(`App Not Found mysqlId ${id}`));
+                reject(new Error(`App Not Found applicationId ${id}`));
             }
         });
     }
