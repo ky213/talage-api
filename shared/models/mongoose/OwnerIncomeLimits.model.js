31c5b9c58bae1808366a819d56b626ca9c9b3b1f
Zach York <zyork@talageins.com>
2021-05-17 12:02:03 -0700

    Refined Owner income limits schema. Removed unnecessary validators, separated some fields, etc.

--- shared/models/mongoose/OwnerIncomeLimits.model.js
+++ shared/models/mongoose/OwnerIncomeLimits.model.js
@@ -9,72 +9,63 @@
 /*jshint esversion: 6 */
 'use strict';
 
-var mongoose = require('mongoose'), Schema = mongoose.Schema;
-// var timestamps = require('mongoose-timestamp'); // zy Is this important?
-var uuid = require('uuid');
-// var mongooseHistory = require('mongoose-history');  // zy Is this important?
+const mongoose = require('mongoose'), Schema = mongoose.Schema;
+const timestamps = require('mongoose-timestamp');
+const uuid = require('uuid');
+const mongooseHistory = require('mongoose-history');
 
-// eslint-disable-next-line no-unused-vars
-const tracker = global.requireShared('./helpers/tracker.js'); // zy Is this important?
+global.requireShared('./helpers/tracker.js');
 
-const incomeLimitValuesSchema = new Schema({
-    effectiveDate: {type: Date, required: [true, 'effective date required']},
-    expirationDate: {type: Date, required: [true, 'expiration date required']},
-    payrollMin: {type: Number, required: false},
-    payrollMax: {type: Number, required: false}
-})
+const IncomeLimitSchema = new Schema({ // zy Make just "IncomeLimitSchema"?
+    effectiveDate: {type: Date, required: false},
+    expirationDate: {type: Date, required: false},
+    payrollLimit: {type: Number, required: false},
+    attributes: {type: Schema.Types.Mixed} // Solves the problem from New York data where construction and non-construction have different limits (given same entity type)
+});
 
-const includedInCoverageSchema = new Schema({ // zy This is certainly wrong but could be a skeleton of something if we wanted to include it at all
-    employeeType: {type: String, enum: ['executive', 'employee', 'manager', 'proprietor']},
-    included: {type: Boolean},
-    canFlipInclusionStatus: {type: Boolean}
-})
+const IncludeExcludeSchema = new Schema({ // zy Review naming
+    able: {type: Boolean, required: true},
+    conditions: {type: Schema.Types.Mixed}
+});
 
-const IncomeLimitsSchema = new Schema({ // zy I think this is a terrible name
+const IncludedInCoverageSchema = new Schema({ // zy This is certainly wrong but could be a skeleton of something if we wanted to include it at all
+    employeeType: {type: String},
+    include: {type: IncludeExcludeSchema},
+    exclude: {type: IncludeExcludeSchema}
+});
+
+const OwnerIncomeLimitsSchema = new Schema({
+    ownerIncomeId : {type: String, required: [true, 'owner income id required'], unique: true},
     insurerId: {type: Number, required: [true, 'insurer id required']},
     state: {type: String, required: [true, 'state required']},
-    entity: {type: String, 
-            enum: [
-                'corporation', 
-                'proprietorship', 
-                'partnership', 
-                'llc', 
-                'non-profit', 
-                'public', 
-                'other', 
-                'trusts-and-estates', 
-                'family-members',
-                'board-of-directors',
-                'employee-election'
-            ]},
+    entityType: {type: String, required: [true, 'entity type required']}, // Do we want entity type to be an array to allow for different entities with the same rules
     attributes: {type: Schema.Types.Mixed},
-    inclusionStatus: {type: [includedInCoverageSchema], required: false, default: []},
-    incomeLimits: {type: [incomeLimitValuesSchema], required: false, default: []}
-})
+    inclusionStatus: {type: [IncludedInCoverageSchema], required: false, default: []},
+    minLimit: {type: [IncomeLimitSchema], required: false, default: []},
+    maxLimit: {type: [IncomeLimitSchema], required: false, default: []}
+});
 
-// MessageSchema.plugin(timestamps); // zy remove?
-// MessageSchema.plugin(mongooseHistory); // zy remove?
+OwnerIncomeLimitsSchema.plugin(timestamps);
+OwnerIncomeLimitsSchema.plugin(mongooseHistory);
 
+OwnerIncomeLimitsSchema.pre('validate', next => {
+    if (this.isNew) {
+        if (!this.ownerIncomeId) {
+            this.ownerIncomeId = uuid.v4();
+        }
+    }
+    next();
+});
 
-// MessageSchema.pre('validate', function(next) { // zy What is this?
-//     if (this.isNew) {
-//         if (!this.messageId) {
-//             this.messageId = uuid.v4();
-//         }
-//     }
-//     next();
-// });
-
-// MessageSchema.pre('save', function(next) { // zy What is this?
-//     next();
-// });
-
+OwnerIncomeLimitsSchema.pre('save', next => {
+    next();
+});
 
 // Configure the 'MessageSchema' to use getters and virtuals when transforming to JSON
-// MessageSchema.set('toJSON', { // zy What is this
+// OwnerIncomeLimitsSchema.set('toJSON', { // zy What does this do?
 //     getters: true,
 //     virtuals: true
 // });
 
 mongoose.set('useCreateIndex', true); // zy What do these do?
-mongoose.model('IncomeLimits', IncomeLimitsSchema);
+mongoose.model('OwnerIncomeLimits', OwnerIncomeLimitsSchema);
