7c7509bc33f4a99a68f073e9a318ee27ad0d38a0
Zach York <zyork@talageins.com>
2021-05-17 16:55:10 -0700

    Reworked how inclusion rules will be stored and cleaned up some comments

--- shared/models/mongoose/OwnerIncomeLimits.model.js
+++ shared/models/mongoose/OwnerIncomeLimits.model.js
@@ -16,31 +16,30 @@ const mongooseHistory = require('mongoose-history');
 
 global.requireShared('./helpers/tracker.js');
 
-const IncomeLimitSchema = new Schema({ // zy Make just "IncomeLimitSchema"?
+const IncomeLimitSchema = new Schema({
     effectiveDate: {type: Date, required: false},
     expirationDate: {type: Date, required: false},
     payrollLimit: {type: Number, required: false},
     attributes: {type: Schema.Types.Mixed} // Solves the problem from New York data where construction and non-construction have different limits (given same entity type)
 });
 
-const IncludeExcludeSchema = new Schema({ // zy Review naming
-    able: {type: Boolean, required: true},
-    conditions: {type: Schema.Types.Mixed}
-});
-
-const IncludedInCoverageSchema = new Schema({ // zy This is certainly wrong but could be a skeleton of something if we wanted to include it at all
+const IncludedInCoverageSchema = new Schema({
     employeeType: {type: String},
-    include: {type: IncludeExcludeSchema},
-    exclude: {type: IncludeExcludeSchema}
+    include: {type: Boolean}, 
+    exclude: {type: Boolean}, 
+    conditions: {type: Schema.Types.Mixed} 
+// zy Rules are difficult to structure. As laid out here we can show whether something can be included or excluded. 
+// The conditions property just shows stated rules, not necessarily what to do about them. 
+// Alternatively, we could drop entire "description" block into a "rules" property on the main schema
 });
 
 const OwnerIncomeLimitsSchema = new Schema({
     ownerIncomeId : {type: String, required: [true, 'owner income id required'], unique: true},
     insurerId: {type: Number, required: [true, 'insurer id required']},
     state: {type: String, required: [true, 'state required']},
-    entityType: {type: String, required: [true, 'entity type required']}, // Do we want entity type to be an array to allow for different entities with the same rules
+    entityType: {type: String, required: [true, 'entity type required']}, // Do we want entity type to be an array to allow for different entities with the same rules?
     attributes: {type: Schema.Types.Mixed},
-    inclusionStatus: {type: [IncludedInCoverageSchema], required: false, default: []},
+    employeeInclusionStatus: {type: [IncludedInCoverageSchema], required: false, default: []},
     minLimit: {type: [IncomeLimitSchema], required: false, default: []},
     maxLimit: {type: [IncomeLimitSchema], required: false, default: []}
 });
