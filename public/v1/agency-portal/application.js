f2f237ff66aa90d96155d1972f3e3a306d621cbb
Raja Singh <raja@talageins.com>
2021-05-24 10:35:22 -0700

    since using status no longer need special logic to check if quoteStatus is dead, originally had to check if mark as dead since we were not updating the status of the quote to dead when we marked as dead

--- public/v1/agency-portal/application.js
+++ public/v1/agency-portal/application.js
@@ -197,15 +197,9 @@ async function getApplication(req, res, next) {
                 }
             }
             quoteJSON.number = quoteJSON.quoteNumber;
-            // Do not overwrite the reasons for quote Obj if it is marked dead, 
-            // mark as dead has reasons "Marked as Dead by XXX (user-email)" that need to be displayed.
-            if(quoteJSON.quoteStatusId !== quoteStatus.dead.id){
-                if (quoteJSON.status === 'bind_requested'
-                || quoteJSON.bound
-                || quoteJSON.status === 'quoted') {
+                if (quoteJSON.status === 'bind_requested'|| quoteJSON.bound || quoteJSON.status === 'quoted') {
                     quoteJSON.reasons = '';
                 }
-            }
             // Change the name of autodeclined
             if (quoteJSON.status === 'autodeclined') {
                 quoteJSON.status = 'Out of Market';
