85a416c3865bc8dbf9c177224dc6c3df3269f6c3
Zach York <zyork@talageins.com>
2021-06-14 15:41:33 -0700

    Fixed which mongoose model was being required

--- uptime/uptime-endpoints.js
+++ uptime/uptime-endpoints.js
@@ -1,5 +1,5 @@
 'use strict';
-const AgencyNetworkModel = require('mongoose').model('WCStateIncomeLimits');
+const AgencyNetworkModel = require('mongoose').model('AgencyNetwork');
 
 /**
  * Gets the current server uptime
