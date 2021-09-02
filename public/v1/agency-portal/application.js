e033883dbc2a9929c56b4066863aae5bc2a4cd22
Brian Pullin <brian@talageins.com>
2021-09-02 15:34:43 -0700

    Removed Arrowhead only check for while getting limits for UI

--- public/v1/agency-portal/application.js
+++ public/v1/agency-portal/application.js
@@ -1442,49 +1442,6 @@ async function GetPolicyLimits(agencyId){
             }
         ]
     };
-    if(agencyId){
-        const arrowHeadInsurerId = 27;
-        // TODO: make this smart logic where we don't do hardcoded check
-        // given an agency grab all of its locations
-        const agencyLocationBO = new AgencyLocationBO();
-        let locationList = null;
-        const query = {"agencyId": agencyId}
-        const getAgencyName = true;
-        const getChildren = true;
-        const useAgencyPrimeInsurers = true;
-        let error = null;
-        locationList = await agencyLocationBO.getList(query, getAgencyName, getChildren, useAgencyPrimeInsurers).catch(function(err){
-            log.error(`Could not get agency locations for agencyId ${agencyId} ` + err.message + __location);
-            error = err;
-        });
-        if(!error){
-            if(locationList && locationList.length > 0){
-                // for each location go through the list of insurers
-                for(let i = 0; i < locationList.length; i++){
-                    if(locationList[i].hasOwnProperty('insurers')){
-                        // grab all the insurers
-                        const locationInsurers = locationList[i].insurers;
-                        if(locationInsurers && locationInsurers.length > 0){
-                            // grab all the insurer ids
-                            const insurerIdList = locationInsurers.map(insurerObj => insurerObj.insurerId);
-                            // are any of the insurer id equal 27 (arrowHead)
-                            if(insurerIdList && insurerIdList.includes(arrowHeadInsurerId)){
-                                limits['BOP'] = [{
-                                    "key": "1000000/1000000/1000000",
-                                    "value": "$1,000,000 / $1,000,000 / $1,000,000"
-                                }];
-                                if(insurerIdList.length > 1){
-                                    log.error(`Arrow Head agency #${agencyId} has other insurers configured for location #${locationList[i].systemId}. Arrow Head agencies should only have 1 insurer configured. Please fix configuration.`);
-                                }
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-    }
     return limits;
 }
 
