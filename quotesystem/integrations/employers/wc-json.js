beeb9a7f7888f90ad9c04dc1cb762585d923e423
Zach York <zyork@talageins.com>
2021-09-21 15:52:40 -0700

    Employers WC - Parse payment plan into standard talageInsurerPaymentPlans

--- quotesystem/integrations/employers/wc-json.js
+++ quotesystem/integrations/employers/wc-json.js
@@ -14,6 +14,7 @@ const Integration = require('../Integration.js');
 const moment_timezone = require('moment-timezone');
 // eslint-disable-next-line no-unused-vars
 const tracker = global.requireShared('./helpers/tracker.js');
+const PaymentPlanSvc = global.requireShared('./services/paymentplansvc.js');
 
 module.exports = class EmployersWC extends Integration {
 
@@ -569,6 +570,48 @@ module.exports = class EmployersWC extends Integration {
                     log.warn(`Appid: ${this.app.id} ${this.insurer.name} ${this.policy.type} Integration Error: Quote structure changed. Unable to find writing company.` + __location);
                 }
             }
+
+            const insurerPaymentPlan = quoteResponse.paymentPlan;
+            try {
+                if (insurerPaymentPlan) {
+                    this.talageInsurerPaymentPlans = [];
+                    const talagePaymentPlan = {};
+                    this.insurerPaymentPlans = insurerPaymentPlan;
+                    const employerPaymentPlanTranslations = {
+                        "DB-D-K": 1, // Employers Billing Option Id - Pay By Code - Pay Option Code
+                        "DB-D-9": 4
+                    };
+                    const lookupKey = `${insurerPaymentPlan.billingOptionId}-${insurerPaymentPlan.payByCode}-${insurerPaymentPlan.payOptionCode}`;
+                    const talagePaymentPlanId = employerPaymentPlanTranslations[lookupKey];
+                    if (!talagePaymentPlanId) {
+                        throw new Error(`Could not match Employers Payment Plan to a Talage Standard Payment Plan. Recommend checking Employers API documentation and determining whether we need to map the given payment plan to a Talage Payment Plan`);
+                    }
+                    talagePaymentPlan.paymentPlanId = talagePaymentPlanId;
+                    talagePaymentPlan.insurerPaymentPlanId = insurerPaymentPlan.billingOptionId;
+                    talagePaymentPlan.insurerPaymentPlanDescription = insurerPaymentPlan.description;
+                    talagePaymentPlan.NumberPayments = insurerPaymentPlan.numberOfInstallments;
+                    talagePaymentPlan.TotalPremium = quoteResponse.totalPremium;
+
+                    let totalInstallmentCost = 0;
+                    if (insurerPaymentPlan.amountDue) { // Amount due per installment if any
+                        talagePaymentPlan.installmentPayment = insurerPaymentPlan.amountDue
+                        totalInstallmentCost = insurerPaymentPlan.amountDue * insurerPaymentPlan.numberOfInstallments;
+                    }
+                    else {
+                        talagePaymentPlan.installmentPayment = 0;
+                    }
+                    talagePaymentPlan.TotalCost = insurerPaymentPlan.downPayment + totalInstallmentCost;
+
+                    talagePaymentPlan.DepositPercent = insurerPaymentPlan.downPaymentPercent;
+                    talagePaymentPlan.DownPayment = insurerPaymentPlan.downPayment;
+                    talagePaymentPlan.invoices = [];
+                    this.talageInsurerPaymentPlans.push(talagePaymentPlan);
+                }
+            }
+            catch (err) {
+                log.error(`${logPrefix}Problem getting payment plan from response object: ${err}` + __location);
+            }
+
 
             try {
                 const quoteLetter = quoteResponse.attachments.find(attachment => attachment.attachmentType === "QuoteLetter");
