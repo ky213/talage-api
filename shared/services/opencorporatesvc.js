39296fd0fddfd398eeb1f3a4d86b2752fb36a6bb
Humberto <humberto.vargas@talageins.com>
2022-03-24 17:06:08 -0600

    Handled the errors from openCorporates service, refactored the getCompanyDetails methods and created a method to get company address using the data endpoint

--- shared/services/opencorporatesvc.js
+++ shared/services/opencorporatesvc.js
@@ -14,7 +14,7 @@ async function performCompanyLookup(companyInfoJSON) {
     //     zipCode: "89502"
     // }
     if(!companyInfoJSON || !companyInfoJSON.name || !companyInfoJSON.state){
-        return null;
+        throw new Error("You must enter at least a company name and state");
     }
 
     const qparms = {
@@ -33,93 +33,125 @@ async function performCompanyLookup(companyInfoJSON) {
     }
     catch (error) {
         // Return a connection error
-        return {error: `OpenCorporate connection error: ${error}`};
+        throw new Error(`OpenCorporates connection error: ${error}`);
     }
     // Ensure we have a successful HTTP status code
     if (OpenCorporateResponse.status !== 200) {
-        log.error(`OpenCorporate returned error status ${OpenCorporateResponse.status}`)
-        return null;
+        throw new Error(`OpenCorporates returned error status ${OpenCorporateResponse.status}`);
     }
     if (!OpenCorporateResponse.data) {
-        log.error(`OpenCorporate no data returned`)
-        return null;
+        throw new Error('OpenCorporate no data returned');
     }
-    let companyInfo = null;
     let businessData = [];
 
     if(OpenCorporateResponse.data?.results?.total_count === 1 && OpenCorporateResponse.data?.results?.companies[0]?.company?.company_number){
-        log.debug('Hard match for Open Corporate');
+        log.debug('Hard hit for Open Corporates');
         try {
-            companyInfo = OpenCorporateResponse.data?.results?.companies[0]?.company;
-            businessData = await handleHardHitMatch(companyInfo);
+            const company = OpenCorporateResponse.data?.results?.companies[0]?.company;
+            const companyDetails = await getCompanyDetails(company, requestOptions);
+            businessData.push(companyDetails);
         }
         catch (error) {
-            log.error(`OpenCorporates Error ${error}` + __location)
-            return null;
+            log.error(`OpenCorporates Error ${error}` + __location);
+            throw new Error(`OpenCorporates connection error: ${error}`);
         }
     }
     else if(OpenCorporateResponse.data?.results?.total_count > 1) {
-        log.debug('Multiple matches for open corporate');
-        const companies = OpenCorporateResponse.data?.results?.companies;
-        businessData = await handleMultipleMatches(companyInfo, companies, requestOptions);
+        log.debug('Multiple matches for Open Corporates');
+        try {
+            const companies = OpenCorporateResponse.data?.results?.companies;
+            businessData = await getCompaniesDetails(companies, requestOptions);
+        }
+        catch(error) {
+            log.error(`OpenCorporates Error ${error}` + __location)
+            throw new Error(`OpenCorporates connection error: ${error}`);
+        }
     }
-
+    log.debug(JSON.stringify(businessData, null, 2));
     return businessData;
-
-    // Generate the generic company info structure
-    //const company = {};
-    // modify company Info instead of making a new company object
-    /* if (companyInfo){
-        if(companyInfo.incorporation_date){
-            log.debug(`OpenCorp Hit Founded Date for ${companyInfo.businessName}`);
-            const foundedDate = moment(companyInfo.incorporation_date,"YYYY-M-D");
-            companyInfo.founded = foundedDate;
-        }
-        if(companyInfo.identifiers?.length > 0){
-            const feinIdentifier = companyInfo.identifiers.find((item) => item.identifier.identifier_system_code === "us_fein");
-            if(feinIdentifier && feinIdentifier.identifier?.uid){
-                log.debug(`OpenCorp Hit FEIN for ${companyInfo.businessName}`);
-                companyInfo.ein = feinIdentifier.identifier.uid;
-            }
-        }
-        companyInfo.businessName = companyInfo.name ? companyInfo.name : '';
-    }*/
 }
 
-async function handleHardHitMatch(companyInfo, requestOptions) {
-    try {
-        const companies = [];
-        const companyUrl = `${openCorporateUrl}/${companyInfo.jurisdiction_code}/${companyInfo.company_number}`;
-        const openCorporateResponse = await axios.get(companyUrl, requestOptions);
-        companyInfo = openCorporateResponse?.data?.results?.company;
-        if (companyInfo) {
-            companies.push(companyInfo);
-        }
-
-        return companies
-    }
-    catch (error) {
-        log.error(`OpenCorporates Error ${error}` + __location)
-        // Return a connection error
-        //return {error: `OpenCorporate connection error: ${error}`};
-        return null;
-    }
-}
-
-async function handleMultipleMatches(companyInfo, companies, requestOptions) {
-    const openCorporateCompanies = await Promise.all(companies.map(async company => {
+async function getCompanyDetails(companyInfo, requestOptions) {
+    if (companyInfo.jurisdiction_code && companyInfo.company_number){
         try {
             const companyUrl = `${openCorporateUrl}/${companyInfo.jurisdiction_code}/${companyInfo.company_number}`;
-            // log.debug(`Opencorporates ${companyUrl} ` + __location)
             const openCorporateResponse = await axios.get(companyUrl, requestOptions);
-            companyInfo = openCorporateResponse?.data?.results?.company
+            const companyDetails = openCorporateResponse?.data?.results?.company;
+            if (companyDetails) {
+                if(companyDetails.incorporation_date){
+                    log.debug(`OpenCorp Hit Founded Date for ${companyDetails.name}`);
+                    log.debug(typeof companyDetails.incorporation_date)
+                    const foundedDate = moment(companyDetails.incorporation_date).tz("America/Los_Angeles").format("YYYY-MM-DD");
+                    companyDetails.founded = foundedDate;
+                }
+                else {
+                    //try to get it from Fernis
+                    companyDetails.founded = '';
+                }
+                if(companyDetails.identifiers?.length > 0){
+                    const feinIdentifier = companyDetails.identifiers.find((item) => item.identifier.identifier_system_code === "us_fein");
+                    if(feinIdentifier && feinIdentifier.identifier?.uid){
+                        log.debug(`OpenCorp Hit FEIN for ${companyDetails.name}`);
+                        companyDetails.ein = feinIdentifier.identifier.uid;
+                    }
+                }
+                else {
+                    // try to get it from HIPAA
+                    companyDetails.ein = '';
+                }
+                if(companyDetails.data?.most_recent?.length > 0){
+                    const mostRecentCompanyData = companyDetails.data?.most_recent;
+                    if (mostRecentCompanyData[0].datum?.data_type === 'CompanyAddress'){
+                        log.debug(`OpenCorp Hit Address for ${companyDetails.name}`);
+                        const dataId = mostRecentCompanyData[0].datum?.id;
+                        await getCompanyAddress(dataId, companyDetails);
+                    }
+                }
+            }
+            return companyDetails;
         }
         catch (error) {
-            log.error(`OpenCorporates ${error} for ${company.name}` + __location)
+            log.error(`OpenCorporates Error ${error}` + __location)
+            throw new Error(`OpenCorporates connection error: ${error}`);
+        }
+    }
+    else {
+        log.error(`At least jurisdiction code and company number needs to be set ${__location}`)
+    }
+}
+
+async function getCompaniesDetails(companies, requestOptions) {
+    const openCorporateCompanies = await Promise.all(companies.map(async companyInfo => {
+        try {
+            const company = companyInfo.company;
+            const companyDetails = await getCompanyDetails(company, requestOptions);
+            return companyDetails;
+        }
+        catch (error) {
+            log.error(`OpenCorporates ${error} for ${companyInfo.company.name}` + __location)
         }
     }));
 
     return openCorporateCompanies;
+}
+
+async function getCompanyAddress(dataId, companyDetails){
+    const companyDataUrl = `https://api.opencorporates.com/v0.4/data/${dataId}`;
+    try {
+        const qparams = {api_token: apiToken};
+        const companyDataResponse = await axios.get(companyDataUrl, {params: qparams});
+        if (companyDataResponse.data?.results?.datum?.attributes){
+            const attributes = companyDataResponse.data.results.datum.attributes
+            companyDetails.address = attributes.street_address;
+            companyDetails.city = attributes.locality;
+            companyDetails.state = attributes.region;
+            companyDetails.zipCode = attributes.postal_code;
+        }
+    }
+    catch(error){
+        log.error(`OpenCorporates data connection error: ${error}`)
+    }
+
 }
 
 
