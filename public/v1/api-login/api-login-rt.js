9f6b81798dfecb0bb0f072c6bc86802ea0649fcb
Joseph Andrew Ortiz <jortiz@talageins.com>
2022-04-04 21:44:00 +0800

    Added validations (permissions & feature json) and error handling to Api Key endpoint

--- public/v1/api-login/api-login-rt.js
+++ public/v1/api-login/api-login-rt.js
@@ -1,6 +1,7 @@
 /* eslint-disable valid-jsdoc */
 const ApiKeyBO = global.requireShared('models/ApiKey-BO.js');
 const ApiKey = new ApiKeyBO();
+const serverHelper = global.requireRootPath('server.js');
 
 /**
  * POST / route
@@ -8,7 +9,25 @@ const ApiKey = new ApiKeyBO();
  * @returns {*}
  */
 async function createApiKeySet(req) {
+    // Check for user permission
+    if(!req.authentication.permissions.api.manage){
+        return serverHelper.forbiddenError('Do not have Permission');
+    }
+
     const userId = parseInt(req.authentication.userID, 10);
+
+    // Check if active keys are at the limit
+    const activeKeysCount = await ApiKey.getActiveKeysCount(userId);
+    if(activeKeysCount >= 5) {
+        throw serverHelper.requestError('Limit Reached (Limit: 5)');
+    }
+
+    // If API Keys feature is not enabled, block deletion
+    const isApiKeysEnabled = await ApiKey.isApiKeysEnabled(userId);
+    if(!isApiKeysEnabled){
+        throw serverHelper.requestError('Feature Disabled');
+    }
+
     return ApiKey.createApiKeySet(userId);
 }
 
@@ -18,7 +37,19 @@ async function createApiKeySet(req) {
  * @returns {*}
  */
 async function getApiKeysForUser(req) {
+    // Check for user permission
+    if(!req.authentication.permissions.api.view){
+        return serverHelper.forbiddenError('Do not have Permission');
+    }
+
     const userId = parseInt(req.authentication.userID, 10);
+
+    // If API Keys feature is not enabled, block fetch
+    const isApiKeysEnabled = await ApiKey.isApiKeysEnabled(userId);
+    if(!isApiKeysEnabled){
+        throw serverHelper.requestError('Feature Disabled');
+    }
+
     return ApiKey.getApiKeysForUser(userId);
 }
 
@@ -28,7 +59,19 @@ async function getApiKeysForUser(req) {
  * @returns {*}
  */
 async function deleteApiKey(req) {
+    // Check for user permission
+    if(!req.authentication.permissions.api.manage){
+        return serverHelper.forbiddenError('Do not have Permission');
+    }
+
     const userId = parseInt(req.authentication.userID, 10);
+
+    // If API Keys feature is not enabled, block deletion
+    const isApiKeysEnabled = await ApiKey.isApiKeysEnabled(userId);
+    if(!isApiKeysEnabled){
+        throw serverHelper.requestError('Feature Disabled');
+    }
+
     return ApiKey.deleteApiKey(userId, req.query.apiKey);
 }
 
@@ -38,7 +81,15 @@ async function deleteApiKey(req) {
  * @returns {*}
  */
 async function authenticateUser(req) {
-    const auth = await ApiKey.authenticate(req.body.apiKey, req.body.apiSecret);
+    const keyId = req.query.apiKey;
+
+    // If API Keys feature is not enabled, block user authentication
+    const isApiKeysEnabled = await ApiKey.isApiKeysEnabled(null, keyId);
+    if(!isApiKeysEnabled) {
+        return {status: 'Disabled'};
+    }
+
+    const auth = await ApiKey.authenticate(keyId, req.body.apiSecret);
 
     if (!auth.isSuccess) {
         return {status: 'failed'};
@@ -55,6 +106,11 @@ const wrapper = (func) => async(req, res, next) => {
         res.send(200, out);
     }
     catch (ex) {
+        // Handling for none Server Errors (Restify Errors)
+        if(ex?.body?.code){
+            log.error(`${ex.body.code}Error: ` + ex.message + __location);
+            return next(ex);
+        }
         log.error("API server error: " + ex + __location);
         res.send(500, ex);
     }
