9cf6c2dea1951ab2e5e4f30866abdaf3d05969c1
carlo macasaet <carlo@talageins.com>
2022-05-13 05:56:30 +0800

    rebuilt api for appl-get

--- public/v1/insurer-portal/applications-rt.js
+++ public/v1/insurer-portal/applications-rt.js
@@ -1,3 +1,5 @@
+const { forEach } = require("lodash");
+
 const serverHelper = global.requireRootPath('server.js');
 
 const ApplicationMongooseModel = global.mongoose.Application;
@@ -6,6 +8,9 @@ const ApplicationMongooseModel = global.mongoose.Application;
 // const ActivityCode = global.mongoose.ActivityCode;
 
 // const InsurerIndustryCodeModel = global.mongoose.InsurerIndustryCode;
+
+const ApplicationBO = global.requireShared('./models/Application-BO.js');
+const AgencyBO = global.requireShared('./models/Agency-BO.js');
 
 /**
  * Responds to get requests for the applications endpoint
@@ -17,23 +22,62 @@ const ApplicationMongooseModel = global.mongoose.Application;
  * @returns {void}
  */
  async function getApplications(req, res, next){
-
-    if(req.params.insurerId) {
-
-        
-        // basic lookup
-        const query = {
+     const query = {
             agencyNetworkId: req.params.insurerId,
         }
         
+    if(req.params.alt) {
+            
+        // basic lookup
         log.debug('application lookup --' + req.params.insurerId);
         try {
-            const appDocs = await ApplicationMongooseModel.find({agencyNetworkId:req.params.insurerId});
+            const appDocs = await ApplicationMongooseModel.find(query);
             res.send(200, appDocs);
         }
         catch (err) {
             log.error(`Application Docs error ` + err + __location);
         }      
+    }
+    else{
+        // traditional lookup
+        log.debug('trad lookup --' + req.params.insurerId);
+        
+        const applicationBO = new ApplicationBO;
+
+        const appDocs = await applicationBO.getAppListForAgencyPortalSearch(query).catch(err=>{
+            log.error('Application Docs-2 error lookup' + err + __location);
+        });
+
+        try {
+            let appList = [];
+            let appDoc = {};
+            const agencyBO = new AgencyBO;
+
+            for(const app in appDocs){
+                appDoc  = appDocs[app]
+                if(appDoc.applicationId){
+                    let appx = await agencyBO.getById(appDoc.agencyId)
+                    // log.debug('agency id ' + JSON.stringify(appx, '', 4));
+                    appDoc.agencyOwnerName = appx.firstName + ' ' + appx.lastName;
+                    appDoc.agencyOwnerEmail = appx.email;
+                    if(appDoc.phone){
+                        appDoc.agencyOwnerPhone = appx.phone;
+                    }else{
+                        appDoc.agencyOwnerPhone = '';
+                    }
+                    if(appDoc.metrics && appDoc.metrics.appValue){
+                        appDoc.appValue = appDoc.metrics.appValue;
+                    }
+
+                    appList.push(appDoc);
+                }
+            }
+
+            res.send(200, appList)
+        }
+        catch (err){
+            log.error('Application finding Agency ' + err + __location);
+        }
     }
 
     return next();
